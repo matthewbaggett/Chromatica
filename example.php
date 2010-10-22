54c8312e5fd3dd7ac3e549730bf04a1ee2d062ae
greyscale <greyscale@2dbfba43-826e-0410-a54b-c69c240d4355>
2010-10-22 10:54:18 +0000

    git-svn-id: http://svn.turbocrms.com/projects/libs/chromatica@614 2dbfba43-826e-0410-a54b-c69c240d4355

--- example.php
+++ example.php
@@ -4,15 +4,15 @@ require_once("chromatica.lib.inc");
 
 $image = new chromaticaImage();
 $image->addLayer(new chromaticaLayer("letters/c.jpg"),0,0);
-$image->addLayer(new chromaticaLayer("letters/h.jpg"),100,0);
+$image->addLayer(new chromaticaLayer("letters/h.png"),100,0);
 $image->addLayer(new chromaticaLayer("letters/r.jpg"),200,0);
 $image->addLayer(new chromaticaLayer("letters/o.jpg"),300,0);
-$image->addLayer(new chromaticaLayer("letters/m.jpg"),400,0);
-$image->addLayer(new chromaticaLayer("letters/a.jpg"),500,0);
-$image->addLayer(new chromaticaLayer("letters/t.jpg"),600,0);
-$image->addLayer(new chromaticaLayer("letters/i.jpg"),700,0);
-$image->addLayer(new chromaticaLayer("letters/c.jpg"),800,0);
-$image->addLayer(new chromaticaLayer("letters/a.jpg"),900,0);
+//$image->addLayer(new chromaticaLayer("letters/m.jpg"),400,0);
+//$image->addLayer(new chromaticaLayer("letters/a.jpg"),500,0);
+//$image->addLayer(new chromaticaLayer("letters/t.jpg"),600,0);
+//$image->addLayer(new chromaticaLayer("letters/i.jpg"),700,0);
+//$image->addLayer(new chromaticaLayer("letters/c.jpg"),800,0);
+//$image->addLayer(new chromaticaLayer("letters/a.jpg"),900,0);
 $image->crunch("img.png",1000,100);
 ?>
 
