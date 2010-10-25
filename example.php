3986a7b8679316f949285d845d8984e06e2e7eae
greyscale <greyscale@2dbfba43-826e-0410-a54b-c69c240d4355>
2010-10-25 14:00:59 +0000

    Fixes transparency
    git-svn-id: http://svn.turbocrms.com/projects/libs/chromatica@616 2dbfba43-826e-0410-a54b-c69c240d4355

--- example.php
+++ example.php
@@ -3,17 +3,21 @@
 require_once("chromatica.lib.inc");
 
 $image = new chromaticaImage();
-$image->addLayer(new chromaticaLayer("letters/c.jpg"),0,0);
-$image->addLayer(new chromaticaLayer("letters/h.png"),100,0);
-$image->addLayer(new chromaticaLayer("letters/r.jpg"),200,0);
-$image->addLayer(new chromaticaLayer("letters/o.jpg"),300,0);
+//$image->addLayer(new chromaticaLayer("letters/c.jpg"),CHROMATICA_TOP_LEFT,0,0);
+$image->addLayer(new chromaticaLayer("letters/h.png"),CHROMATICA_TOP_LEFT,100,0);
+//$image->addLayer(new chromaticaLayer("letters/r.jpg"),CHROMATICA_TOP_LEFT,200,0);
+//$image->addLayer(new chromaticaLayer("letters/o.jpg"),CHROMATICA_TOP_LEFT,300,0);
+
 //$image->addLayer(new chromaticaLayer("letters/m.jpg"),400,0);
 //$image->addLayer(new chromaticaLayer("letters/a.jpg"),500,0);
 //$image->addLayer(new chromaticaLayer("letters/t.jpg"),600,0);
 //$image->addLayer(new chromaticaLayer("letters/i.jpg"),700,0);
 //$image->addLayer(new chromaticaLayer("letters/c.jpg"),800,0);
 //$image->addLayer(new chromaticaLayer("letters/a.jpg"),900,0);
+
+$image->addLayer(new chromaticaText("Copyright lol",CHROMATICA_WHITE),CHROMATICA_BOTTOM_RIGHT,50,50);
 $image->crunch("img.png",1000,100);
 ?>
-
-<img src="img.png"/>
+<body bgcolor="#FFCC99">
+	<img src="img.png"/>
+</body>
